08981e1d16931d873a5099505647d587d26ea20a
WitBun <mrkrixus@gmail.com>
2024-04-25 23:51:04 +0200

    bulk of site completed

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -4,4 +4,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
         const date= new Date().getFullYear();
         footerText.innerHTML = date.toString();
     }
+
+    let formDate = document.getElementById("date");
+    const date = new Date();
+    const jsIsRetardedInDateStrings = date.toISOString().substring(0,10);
+    if(formDate){
+        formDate.value = jsIsRetardedInDateStrings;
+        formDate.min = jsIsRetardedInDateStrings;
+    }
 })
